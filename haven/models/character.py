b5789beb0b83401af256f004e3d56f6379bf88bc
Michael Bossner <Michael.bossner@hotmail.com>
2021-10-05 15:11:53 +1000

    v0.2.13
    fixed again same bug

--- haven/models/character.py
+++ haven/models/character.py
@@ -67,7 +67,8 @@ class Trackable(models.Model):
 class CharacterManager(models.Manager):
     def create_partial_character(self, splatSlug, user, json, guild_info):
         splat = Splat.objects.get(slug=splatSlug)
-        guild, member = None
+        guild = None
+        member = None
 
         if (guild_info['id']):
             guild, created = Chronicle.objects.update_or_create(
