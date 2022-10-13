a8d891950944e81b3bf14740f5a1837c3c633aaa
Miki <Michael.Bossner@hotmail.com>
2022-10-13 14:14:34 +1000

    v0.7.8
    Trying to fix settings bug

--- rod/asgi.py
+++ rod/asgi.py
@@ -12,9 +12,8 @@ import os
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-import gateway.routing
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rod.settings')
+import gateway.routing
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
