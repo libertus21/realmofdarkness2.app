a105bffbd192a83baf1909d4fdb6e743fd2fa264
Mirai-Miki <michael.bossner@hotmail.com>
2023-10-18 14:07:33 +1000

    v1.1.18
    - added print to try and see what is going on

--- haven/serializers/validation_error_handler.py
+++ haven/serializers/validation_error_handler.py
@@ -5,7 +5,9 @@ from rest_framework import serializers, status
 def validation_error_handler(errors):
   error = errors.get('name', None)
   if error is not None and len(error): error = error[0]
-  else: Response(status=status.HTTP_400_BAD_REQUEST)
+  else: 
+    print(errors)
+    Response(status=status.HTTP_400_BAD_REQUEST)
 
   if getattr(error, 'code', 'invalid') != 'invalid':
     return Response(error, status=error.code)
