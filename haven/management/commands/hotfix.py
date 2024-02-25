323f7ff39ec06de1f69301668bc99f20097f86c8
Michael Bossner <michael.bossner@hotmail.com>
2024-02-25 18:54:09 +1000

    v1.3.5 (7)

--- haven/management/commands/hotfix.py
+++ haven/management/commands/hotfix.py
@@ -9,7 +9,7 @@ class Command(BaseCommand):
             updated_disciplines = {}
             for discipline, data in vampire.disciplines.items():
                 updated_disciplines[discipline] = data
-                updated_disciplines['powers'] = {'1': None, '2': None, '3': None, '4': None, '5': None}
+                updated_disciplines[discipline]['powers'] = {'1': None, '2': None, '3': None, '4': None, '5': None}
             vampire.disciplines = updated_disciplines
-            self.stdout.write(f"Updated disciplines for {vampire.name}\n{updated_disciplines}")
+            self.stdout.write(f"Updated disciplines for {vampire.name}\n{vampire.disciplines}")
 
