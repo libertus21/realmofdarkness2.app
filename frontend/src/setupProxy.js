f6f90e343c81170fbe5eeb59575a3c5a0ca94ad7
Mirai-Miki <Mirai-Miki@hotmail.com>
2025-04-08 14:11:22 +1000

    deps/update_packages

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,12 +1,16 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
-  app.use(
-    "/api",
-    createProxyMiddleware({
-      target: "http://localhost:8080", // Replace with your Django server URL
-      changeOrigin: true,
-      secure: false,
-    })
-  );
+  // Common proxy options
+  const proxyOptions = {
+    target: "http://localhost:8080", // Replace with your Django server URL
+    changeOrigin: true,
+    secure: false,
+  };
+
+  // Proxy API requests
+  app.use("/api", createProxyMiddleware(proxyOptions));
+
+  // Proxy media requests
+  app.use("/media", createProxyMiddleware(proxyOptions));
 };
